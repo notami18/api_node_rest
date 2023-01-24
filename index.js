c66e3f41a318baca5cd124a2fe02f1cdaa90b1c6
notami18 <andresnotami@gmail.com>
2023-01-24 16:55:37 -0500

    🚧: GET: parámetros query

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const faker = require('faker');
+
 const app = express();
 const port = 3000;
 
@@ -11,16 +13,36 @@ app.get('/nueva-ruta', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-  res.json([
-    { name: 'product 1', price: 1000 },
-    { name: 'product 2', price: 2000 },
-    { name: 'product 3', price: 3000 },
-  ]);
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+  }
+  res.json(products);
+});
+
+// TODO: Los endpoints especificos deben declararsen antes de los endpoints dinamicos. Uno de los mandamientos.
+app.get('/products/filter', (req, res) => {
+  res.send(`Yo soy un filter`);
 });
 
 app.get('/product/:id', (req, res) => {
   const { id } = req.params;
   res.json({ id, name: 'product 2', price: 2000 });
+});
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({ limit, offset });
+  } else {
+    res.send('No hay parametros');
+  }
 });
 
 app.get('/categories/:categoryId/products/:productId', (req, res) => {
