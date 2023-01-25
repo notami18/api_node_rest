ee694103716aad3970f7137701de891c839a743c
notami18 <andresnotami@gmail.com>
2023-01-24 19:25:54 -0500

    🚧: PUT, PATCH y DELETE

--- routes/products.router.js
+++ routes/products.router.js
@@ -30,8 +30,26 @@ router.post('/', (req, res) => {
   const body = req.body;
   res.json({
     message: 'created',
-    data: body
-  })
-})
+    data: body,
+  });
+});
 
-module.exports = router
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update',
+    data: body,
+    id,
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'deleted',
+    id,
+  });
+});
+
+module.exports = router;
