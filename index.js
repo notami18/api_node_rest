d5a9d2468182df84486786165d4bd2a02a4579ca
notami18 <andresnotami@gmail.com>
2023-01-26 08:30:24 -0500

    📦️: add swagger

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const routerApi = require('./routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
-const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require('./middlewares/error.handler');
 
 const app = express();
 const port = 3000;
@@ -21,6 +27,8 @@ routerApi(app);
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`my port ${port}`);
